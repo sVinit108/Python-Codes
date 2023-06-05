fb997d90fae3bdc0580d780f61c4b2df1c845bd4
Vinit <sharmavinit.1008@gmail.com>
2023-06-05 12:27:46 +0530

    Walrus Updation

--- Cpt 2 - Basics/Walrus_Operator.py
+++ Cpt 2 - Basics/Walrus_Operator.py
@@ -7,6 +7,7 @@
 #   foods.append(food)
 
 # walrus operator (:=)
+# It lets you assign a value to a variable right inside the condition of an if statement or while loop.
 foods = list()
 while (food := input("What food do you like?: ")) != "quit":
     foods.append(food)
